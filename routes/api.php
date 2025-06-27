42a8244d11f5824a483a3ca4dcab3b62b61b00b0
goldistry <goldistry99@gmail.com>
2025-06-27 21:28:22 +0700

    FEAT: detail modal for question, answer, comment in activity (BETA: comment e ga mau ke sendddd)

--- routes/api.php
+++ routes/api.php
@@ -103,7 +103,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
         $questions = Question::select('id', 'title', 'question')->get();
         return response()->json($questions);
     });
-
 });
 
 // ==========================
@@ -133,6 +132,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
             Route::post('/users/{user}/unblock', [AdminUserController::class, 'unblockUser'])->name('users.unblock');
             Route::get('/users', [AdminUserController::class, 'getBasicUserInfo'])->name('api.admin.users.index');
             Route::get('/users/{user}/activity', [AdminUserController::class, 'getActivitySummary']);
+            Route::get('/content-detail/{type}/{id}', [AdminUserController::class, 'getContentDetail'])->name('content.detail');
         });
 
         Route::middleware('role.admin:community-manager,super-admin')->group(function () {
